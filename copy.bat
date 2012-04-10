79f23b55680f8994e300ce81a40497ff2dec2135
Andrew Hall <alh17885@googlemail.com>
2012-04-10 16:32:31 +0100

    removal of files

--- copy.bat
+++
@@ -1,3 +0,0 @@
-XCOPY C:\Users\Andy\Dropbox\Repo\c2\behaviors "C:\Program Files\Construct 2\exporters\html5\behaviors" /S
-
-XCOPY C:\Users\Andy\Dropbox\Repo\c2\plugins "C:\Program Files\Construct 2\exporters\html5\plugins" /S
--- copywork.bat
+++
@@ -1,3 +0,0 @@
-XCOPY C:\Users\halla\Dropbox\Repo\c2\behaviors "C:\Users\halla\AppData\Local\Construct 2\exporters\html5\behaviors" /S
-
-XCOPY C:\Users\halla\Dropbox\Repo\c2\plugins "C:\Users\halla\AppData\Local\Construct 2\exporters\html5\plugins" /S
